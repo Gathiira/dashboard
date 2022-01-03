cda06234b5655d818b93dac312da2bf9cf4e8243
Gathiira <mwangyjose@gmail.com>
2022-01-03 22:24:42 +0300

    featured item cards

--- src/components/FeatureInfo.jsx
+++ src/components/FeatureInfo.jsx
@@ -3,20 +3,20 @@ import {MdOutlineArrowDownward, MdOutlineArrowUpward} from 'react-icons/md'
 
 const FeatureInfo = () => {
     return (
-        <div className='mt-5 flex flex-col gap-2 md:flex-row 
-                        justify-around items-center overflow-x-auto w-full'>
-            <div className="flex-[1] flex-col gap-2 bg-gray-200 p-5 rounded-lg">
+        <div className='m-5 flex flex-col gap-2 md:flex-row 
+                        justify-around items-center overflow-auto'>
+            <div className="flex-[1] flex-col gap-2 bg-gray-200  m-2 p-5 rounded-lg cursor-pointer shadow-lg shadow-gray-800">
                 <span className="text-3xl">Revenue</span>
                 <div className="flex flex-col gap-2">
                     <span className="text-bold text-2xl text-black">$2,232</span>
                     <div className='flex gap-2 items-center'>
-                        <span className="featuredMoneyRate"> -5.03 </span>
+                        <span className="featuredMoneyRate"> - 5.03 </span>
                         <MdOutlineArrowDownward/>
                     </div>
                 </div>
                 <small className="text-xs">Compared to last month</small>
             </div>
-            <div className="flex-[1] flex-col gap-2 bg-gray-200 p-5 rounded-lg">
+            <div className="flex-[1] flex-col gap-2 bg-gray-200 m-2 p-5 rounded-lg cursor-pointer shadow-lg shadow-gray-800">
                 <span className="text-3xl">Demand</span>
                 <div className="flex flex-col gap-2">
                     <span className="text-bold text-2xl text-black">$15,974</span>
@@ -27,18 +27,18 @@ const FeatureInfo = () => {
                 </div>
                 <small className="text-xs">Compared to last month</small>
             </div>
-            <div className="flex-[1] flex-col gap-2 bg-gray-200 p-5 rounded-lg">
+            <div className="flex-[1] flex-col gap-2 bg-gray-200 m-2 p-5 rounded-lg cursor-pointer shadow-lg shadow-gray-800">
                 <span className="text-3xl">Sales</span>
                 <div className="flex flex-col gap-2">
                     <span className="text-bold text-2xl text-black">$4,549</span>
                     <div className='flex gap-2 items-center'>
-                        <span className="featuredMoneyRate"> -6.03 </span>
+                        <span className="featuredMoneyRate"> - 6.03 </span>
                         <MdOutlineArrowDownward/>
                     </div>
                 </div>
                 <small className="text-xs">Compared to last month</small>
             </div> 
-            <div className="flex-[1] flex-col gap-2 bg-gray-200 p-5 rounded-lg">
+            <div className="flex-[1] flex-col gap-2 bg-gray-200 m-2 p-5 rounded-lg cursor-pointer shadow-lg shadow-gray-800">
                 <span className="text-3xl">Cost</span>
                 <div className="flex flex-col gap-2">
                     <span className="text-bold text-2xl text-black">$11,646</span>
